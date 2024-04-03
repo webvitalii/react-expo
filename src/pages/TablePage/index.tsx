384143635fb38085eae4d35778a1d7a99e0632ea
Vitalii <vitalii.skorokhod@gmail.com>
2024-04-03 11:48:07 -0400

    Added simple table

--- src/pages/TablePage/index.tsx
+++ src/pages/TablePage/index.tsx
@@ -1,7 +1,48 @@
-// import { Post } from "../interfaces/Post";
+import { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../components/ui/table";
+import { Post } from "../../interfaces/Post";
 
 const TablePage = () => {
-  return <section className="TablePage">Table page</section>;
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error("Error fetching posts:", error));
+  }, []);
+
+  return (
+    <section className="TablePage">
+      <Table>
+        <TableCaption>A list of your recent posts.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">User ID</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Body</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {posts.map((post) => (
+            <TableRow key={post.id}>
+              <TableCell className="font-medium">{post.userId}</TableCell>
+              <TableCell>{post.title}</TableCell>
+              <TableCell>{post.body}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </section>
+  );
 };
 
 export default TablePage;
