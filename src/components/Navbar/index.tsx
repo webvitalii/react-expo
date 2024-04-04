59c0f8c1ca6f2228ffce6f8fedea3d58348c378f
Vitalii <vitalii.skorokhod@gmail.com>
2024-04-04 13:55:26 -0400

    Optimized Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,29 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+const navLinks = [
+  { to: "/", text: "Home" },
+  { to: "/table", text: "Table" },
+  { to: "/table-advanced", text: "Table advanced" },
+];
 
 const Navbar: React.FC = () => {
   return (
     <nav className="flex justify-center space-x-4">
-      <Link
-        to="/"
-        className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-      >
-        Home
-      </Link>
-
-      <Link
-        to="/table"
-        className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-      >
-        Table
-      </Link>
-
-      <Link
-        to="/table-advanced"
-        className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-      >
-        Table advanced
-      </Link>
+      {navLinks.map((link) => (
+        <NavLink
+          key={link.to}
+          to={link.to}
+          className={({ isActive }) =>
+            isActive
+              ? "font-bold px-3 py-2 text-slate-900 rounded-lg bg-slate-200"
+              : "font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+          }
+        >
+          {link.text}
+        </NavLink>
+      ))}
     </nav>
   );
 };
