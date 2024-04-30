111ec28bb834fdf2bde498c06b7d627c103169f1
Vitalii <vitalii.skorokhod@gmail.com>
2024-04-30 17:01:51 -0400

    Added counter with redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
+import { Provider } from "react-redux";
+import { store } from "@/state/store.ts";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import TablePage from "./pages/TablePage";
@@ -10,19 +12,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/table" element={<TablePage />} />
-          <Route path="/form" element={<FormPage />} />
-          <Route path="/carousel" element={<CarouselPage />} />
-          <Route path="/counter" element={<CounterPage />} />
-        </Routes>
-      </Router>
-      <Toaster />
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/table" element={<TablePage />} />
+            <Route path="/form" element={<FormPage />} />
+            <Route path="/carousel" element={<CarouselPage />} />
+            <Route path="/counter" element={<CounterPage />} />
+          </Routes>
+        </Router>
+        <Toaster />
+      </div>
+    </Provider>
   );
 }
 
--- src/pages/CounterPage/index.tsx
+++ src/pages/CounterPage/index.tsx
@@ -1,7 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/state/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+} from "@/state/counter/counterSlice";
+import { Button } from "@/components/ui/button";
+
 const CounterPage = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <section className="CounterPage">
-      <h1 className="text-2xl font-bold mb-4">Counter</h1>
+      <h1 className="text-2xl font-bold mb-4">Counter: {count}</h1>
+
+      <div>
+        <Button onClick={() => dispatch(decrement())} className="mr-3">
+          -1
+        </Button>
+        <Button onClick={() => dispatch(increment())} className="mr-3">
+          +1
+        </Button>
+        <Button onClick={() => dispatch(incrementByAmount(5))} className="mr-3">
+          +5
+        </Button>
+        <Button onClick={() => dispatch(incrementAsync(10))} className="mr-3">
+          +10 async (3 seconds)
+        </Button>
+      </div>
     </section>
   );
 };
