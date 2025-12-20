7f801d125deb41d7d7f86e8e09ff7ebe770ddb65
Vitalii <vitalii.skorokhod@gmail.com>
2025-12-19 21:37:29 -0500

    Use Vite instead of PostCSS for Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +12,12 @@ export default defineConfig({
       target: 'react',
       autoCodeSplitting: true,
     }),
-    react(),
+    viteReact(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
