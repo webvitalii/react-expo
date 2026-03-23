d3af6e57b141e5999e0b0c42c5cf0a8655145fa6
Vitalii <vitalii.skorokhod@gmail.com>
2026-03-23 01:45:13 -0400

    Removed support notice console.log

--- src/i18n.ts
+++ src/i18n.ts
@@ -24,6 +24,7 @@ i18n
     supportedLngs: supportedLanguages,
     ns: ['navbar'],
     defaultNS: 'navbar',
+    showSupportNotice: false,
     backend: {
       loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
