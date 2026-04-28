3969a10b38e2f14e7059d78ee7256926fd241643
Vitalii <vitalii.skorokhod@gmail.com>
2026-04-28 18:58:00 -0400

    Added Vite code splitting config

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,28 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'react',
+              test: /node_modules[\\/](react|react-dom|scheduler)[\\/]/,
+              priority: 30,
+            },
+            { name: 'base-ui', test: /node_modules[\\/]@base-ui[\\/]/, priority: 25 },
+            { name: 'tanstack', test: /node_modules[\\/]@tanstack[\\/]/, priority: 20 },
+            {
+              name: 'i18n',
+              test: /node_modules[\\/](i18next|react-i18next|i18next-http-backend)[\\/]/,
+              priority: 20,
+            },
+            { name: 'zod', test: /node_modules[\\/]zod[\\/]/, priority: 20 },
+            { name: 'vendor', test: /node_modules/, priority: 10 },
+          ],
+        },
+      },
+    },
+  },
 });
